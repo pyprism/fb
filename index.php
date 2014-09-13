faadefbf4cd05450817c9460e1747f0e64e9537f
pyprism <git.pyprism@gmail.com>
2014-09-13 17:03:53 +0600

    :(

--- index.php
+++ index.php
@@ -1,56 +1,82 @@
 <?php
 
+session_start();
+
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
 require 'vendor/autoload.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookRequest;
+
 $login = file_get_contents("login.json");
 $log_json = json_decode($login,true);
 
 //echo $log_json['app_id'] ;
 
-$config = array(
-    'appId' => '721830601168795',
-    'secret' => 'ad40de14362bf1b4152d2f128dcc640a',
-    'allowSignedRequest' => false // optional but should be set to false for non-canvas apps
-  );
+FacebookSession::setDefaultApplication($log_json['app_id'], $log_json['app_secret']);
 
-  $facebook = new Facebook($config);
-  $user_id = $facebook->getUser();
-?>
-<html>
-  <head></head>
-  <body>
+$helper = new FacebookRedirectLoginHelper('http://apps.dev/');
 
-  <?php
-    if($user_id) {
+try
+{
+    // In case it comes from a redirect login helper
+    $session = $helper->getSessionFromRedirect();
+}
+catch( FacebookRequestException $ex )
+{
+    // When Facebook returns an error
+    echo $ex;
+}
+catch( Exception $ex )
+{
+    // When validation fails or other local issues
+    echo $ex;
+}
 
-      // We have a user ID, so probably a logged in user.
-      // If not, we'll get an exception, which we handle below.
-      try {
-
-        $user_profile = $facebook->api('/me/friendrequests/','GET');
-        //echo "Name: " . $user_profile['name'];
-        var_dump($user_profile) ;
-
-      } catch(FacebookApiException $e) {
-        // If the user is logged out, you can have a
-        // user ID even though the access token is invalid.
-        // In this case, we'll get an exception, so we'll
-        // just ask the user to login again here.
-        $login_url = $facebook->getLoginUrl(array(
-                       'scope' => 'read_friendlists'
-                       ));
-        echo 'Please <a href="' . $login_url . '">login.</a>';
-        error_log($e->getType());
-        error_log($e->getMessage());
-      }
-    } else {
-
-      // No user, print a link for the user to login
-      $login_url = $facebook->getLoginUrl();
-      echo 'Please <a href="' . $login_url . '">login.</a>';
-
+// see if we have a session in $_Session[]
+if( isset($_SESSION['token']))
+{
+    // We have a token, is it valid?
+    $session = new FacebookSession($_SESSION['token']);
+    try
+    {
+        $session->Validate($log_json['app_id'] ,$secret);
     }
+    catch( FacebookAuthorizationException $ex)
+    {
+        // Session is not valid any more, get a new one.
+        $session ='';
+    }
+}
 
-  ?>
+// see if we have a session
+if ( isset( $session ) )
+{
+    // set the PHP Session 'token' to the current session token
+    $_SESSION['token'] = $session->getToken();
+    // SessionInfo
+    $info = $session->getSessionInfo();
+    // getAppId
+    echo "Appid: " . $info->getAppId() . "<br />";
+    // session expire data
+    $expireDate = $info->getExpiresAt()->format('Y-m-d H:i:s');
+    echo 'Session expire time: ' . $expireDate . "<br />";
+    // session token
+    echo 'Session Token: ' . $session->getToken() . "<br />";
+}
+else
+{
+    // show login url
+    echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
+}
+$session = new FacebookSession($_GET["code"]);
 
-  </body>
-</html>
+$request = new FacebookRequest($session, 'GET', '/me');
+$response = $request->execute();
+$graphObject = $response->getGraphObject();
+
+echo $graphObject;
